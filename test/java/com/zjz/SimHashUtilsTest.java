f0a8aa10a9a5bd70c697eea646358f583d1a4761
zjz <1053970107@qq.com>
2023-09-16 17:46:21 +0800

    fix(SimHashUtilsTest)：修改测试案例

--- test/java/com/zjz/SimHashUtilsTest.java
+++ test/java/com/zjz/SimHashUtilsTest.java
@@ -10,7 +10,7 @@ public class SimHashUtilsTest {
     //获取字符串Hash测试
     @Test
     public void getHashTest() {
-        String[] strings = {"余华", "是", "一位", "值得尊重", "的", "作家"};
+        String[] strings = {"我们", "说", "如来", "他", "真的", "来","了吗"};
         for (String string : strings) {
             String stringHash = SimHashUtils.getHash(string);
             System.out.println(stringHash.length());
@@ -22,8 +22,8 @@ public class SimHashUtilsTest {
     //获取字符串SimHash测试
     @Test
     public void getSimHashTest() {
-        String orig = FileUtils.readTxt("F:\\DeskTop\\查重测试文件\\orig.txt");
-        String orig_add = FileUtils.readTxt("F:\\DeskTop\\查重测试文件\\orig_0.8_add.txt");
+        String orig = FileUtils.readTxt("D:\\3121005063\\src\\main\\resources\\file\\orig.txt");
+        String orig_add = FileUtils.readTxt("D:\\3121005063\\src\\main\\resources\\file\\orig_0.8_add.txt");
         System.out.println(orig + ": \n" + SimHashUtils.generateSimHash(orig));
         System.out.println("=====================");
         System.out.println(orig_add + ": \n" + SimHashUtils.generateSimHash(orig_add));
